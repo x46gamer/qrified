69557ad0d8acef390b417e21a6e420d68a3deb10
Your Name <your.email@example.com>
2025-06-11 20:26:03 +0100

    Huge Update58

--- src/pages/lifetime.tsx
+++ src/pages/lifetime.tsx
@@ -142,7 +142,7 @@ const LifetimePage = () => {
     }
     try {
       setIsLoading(true);
-      const sessionId = await createCheckoutSession('prod_STZYwQwtObNE8T');
+      const sessionId = await createCheckoutSession('prod_STjyRAv61Ylnaf');
       const stripe = await getStripe();
       if (!stripe) throw new Error('Failed to load Stripe');
 
@@ -164,7 +164,7 @@ const LifetimePage = () => {
     }
     try {
       setIsLoading(true);
-      const sessionId = await createCheckoutSession('prod_STZYwQwtObNE8T');
+      const sessionId = await createCheckoutSession('prod_STjyRAv61Ylnaf');
       const stripe = await getStripe();
       if (!stripe) throw new Error('Failed to load Stripe');
 
